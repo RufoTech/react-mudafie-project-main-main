7e3bafc09d9d0c543657eab73f0ced319b167ebe
Rufat-Alakbarli <alekberovrufa@gmail.com>
2024-07-31 16:04:08 +0400

    sa

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -10,9 +10,8 @@ const Introduction: React.FC = () => {
       <div className="introduction-about">
         <Swiper
           spaceBetween={0}
-          pagination={{
-            clickable: true,
-          }}
+          
+      
           autoplay={{
             delay: 2500,
             disableOnInteraction: false,
@@ -27,14 +26,21 @@ const Introduction: React.FC = () => {
               <div className="container">
                 <span className='intro-h1'>
                   <h1 className='playfair-display intro-h1'>Udfort Luxury Residences</h1>
+                  <div className="social-media-introduction d-flex justify-content-end">
+                    <h6 className='text-light'>SOCIAL MEDIA ------</h6>
+                    
+                  </div>
                 </span>
                 <h2 className='text-light roboto-bold intro-h2 py-4'>Living Spaces In Pecherska-Kiev</h2>
+               
                 <div className="intro-button py-4">
                   <Button />
                   <img src="/assets/images/introduction/service-icon-1.png" alt="" className='intro-icon1' />
                 </div>
               </div>
+
             </div>
+           
           </SwiperSlide>
 
           <SwiperSlide>
@@ -68,6 +74,7 @@ const Introduction: React.FC = () => {
           </SwiperSlide>
 
         </Swiper>
+        
       </div>
     </>
   )
